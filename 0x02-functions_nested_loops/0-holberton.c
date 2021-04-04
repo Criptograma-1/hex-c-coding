f7eb113a9d5102e4833d74e97ba0d8b29eb1dff2
root <root@DESKTOP-7JEPPV5.localdomain>
2021-04-04 12:10:15 -0300

    Add SubDir 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "Holberton";
+
+	putchar (str);
+	putchar ('\n');
+	return (0);
+}
