8d9561e803963f08a896492397086d0d6f1a89ef
root <root@DESKTOP-7JEPPV5.localdomain>
2021-04-04 12:49:55 -0300

    Add files from exercise 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - a function that prints the alphabet 10 times, in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int alp;
+	int cont;
+
+	for (cont = 0 ; cont < 10 ; cont++)
+	{
+		for (alp = 'a' ; alp <= 'z' ; alp++)
+		{
+			_putchar(alp);
+		}
+	_putchar('\n');
+	}
+}
