e9e226b2a0ad9b41846e6558a8fbf6c2c8abc10e
Criptograma-1 <81261737+Criptograma-1@users.noreply.github.com>
2021-04-04 00:12:35 -0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,16 +9,19 @@
 int main(void)
 {
 	int n;
-	int last;
-	
+	int L;
+	char str[] = "Last digit of";
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last = n % 10;
-	if (n > 5)
-		printf("last digit of %d is %d and is greater than 5\n", n, last);
-	else if (n == 0)
-		printf("last digit of %d is %d and is 0\n", n, last);
-	else if (n < 6)
-		printf("last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	L = n % 10;
+
+	if (L > 5)
+		printf("%s %d is %d and is greater than 5\n", str, n, L);
+	else if (L == 0)
+		printf("%s %d is %d and is 0\n", str, n, L);
+	else if (L < 6)
+		printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
+
 	return (0);
 }
