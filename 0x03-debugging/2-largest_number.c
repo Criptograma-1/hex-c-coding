ade93f022ab7b6254532c1cd9fa195466435d33d
Criptograma-1 <81261737+Criptograma-1@users.noreply.github.com>
2021-04-05 18:47:40 -0300

    Fix 3

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,7 +10,7 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a >= b && b >= c)
 	{
 		largest = a;
 	}
@@ -18,7 +18,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if (c > a && a > b)
+	else
 	{
 		largest = c;
 	}
