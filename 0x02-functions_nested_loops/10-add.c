92a2fe72864c1c2227f6187e9d3de15e3b151ef0
root <root@DESKTOP-7JEPPV5.localdomain>
2021-04-05 16:11:21 -0300

    Add a+b files

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * add - function that adds two integers and returns the result.
+ * a, b: numbers input
+ * Return: a+b result
+ */
+int add(int a, int b)
+{
+	return(a + b);
+}
