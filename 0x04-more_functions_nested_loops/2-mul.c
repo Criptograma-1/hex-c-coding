d2e97257aa5ac2c2eb3ca2081ecff67d2a9aaa78
Criptograma-1 <andresa.info@gmail.com>
2021-04-08 16:10:51 +0000

    Add taks 2 files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* _mul - a function that multiplies two integers.
+* @a: type int.
+* @b: type int.
+* Return: result.
+*/
+int mul(int a, int b)
+{
+	int result;
+
+	result=a*b;
+	return (result);
+}
