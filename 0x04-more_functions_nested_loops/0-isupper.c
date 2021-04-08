648944e99ba5129adfc996730be2437c0363fcef
Criptograma-1 <andresa.info@gmail.com>
2021-04-08 15:45:46 +0000

    Add task 1 files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+* _isupper - a function that checks for uppercase character
+* @c: single letter input
+* Return 1 if int c is uppercase, 0 ir otherwise
+*/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
