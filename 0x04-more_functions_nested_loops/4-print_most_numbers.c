d9c7a78b3f0620a4ca55c582c58c83debdbaa4be
Criptograma-1 <andresa.info@gmail.com>
2021-04-08 16:38:25 +0000

    Add task 4 files

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* print_most_numbers - prints the numbers from 0 to 9, without 2 & 4
+*
+* Return: Always 0 (Success)
+*/
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = '0' ; num <= '9' ; num++)
+	{
+		if (num != '2' && num != '4')
+			_putchar(num);
+	}
+	_putchar('\n');
+}
