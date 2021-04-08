9f542aa337c9a5e0b8feaf366b31a23c4004edc3
root <root@DESKTOP-7JEPPV5.localdomain>
2021-04-08 15:30:20 -0300

    Add tasks 9 and 10 files

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ * Return: Nothing
+ */
+void print_triangle(int size)
+{
+	int i, j, s;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				s = size - i - 1;
+				if (j < s)
+					_putchar(' ');
+				else
+					_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
