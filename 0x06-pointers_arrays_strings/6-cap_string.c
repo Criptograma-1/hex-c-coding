25a58c6544e9559cc652d3e574deeb97acf57265
Criptograma-1 <andresa.info@gmail.com>
2021-04-09 23:03:06 +0000

    Add task 6 giles

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+* cap_string - a function that capitalizes all words of a string
+* @s: string
+* return: s
+*/
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= 32;
+	while (s[i] != '\0')
+	{
+		if ((s[i - 1] == ' ' || s[i - 1] == '\n'
+		|| s[i - 1] == '\t' || s[i - 1] == ','
+		|| s[i - 1] == '?' || s[i - 1] == '"'
+		|| s[i - 1] == ';' || s[i - 1] == '!'
+		|| s[i - 1] == '(' || s[i - 1] == ')'
+		|| s[i - 1] == '{' || s[i - 1] == '}'
+		|| s[i - 1] == '.')
+		&& (s[i] > 'a' && s[i] < 'z'))
+		{
+			s[i] -= 32;
+		}
+		else
+			s[i] = s[i];
+		i++;
+	}
+	return (s);
+}
