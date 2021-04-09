b88d8929149c1a684528c93fc09b63b398460dce
Criptograma-1 <andresa.info@gmail.com>
2021-04-09 20:40:17 +0000

    fix task 3

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -1,5 +1,6 @@
-#include "holberton.h"
 #include <stdio.h>
+
+int _strcmp(char *, char *);
 
 /**
  * main - check the code for Holberton School students.
@@ -8,11 +9,9 @@
  */
 int main(void)
 {
-    char s1[] = "Hello";
-    char s2[] = "World!";
+	char s1[] = "Holberton";
+	char s2[] = "Holbeerton";
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
-    return (0);
+	printf("%d\n", _strcmp(s1, s2));
+	return (0);
 }
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,9 +7,18 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, aux;
-
-	i = j = 0;
-	aux = s1[i]-s2[j];
-	return (aux);
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 > *s2)
+		{
+			return (*s1 - *s2);
+		}
+		else if (*s1 < *s2)
+		{
+			return (*s1 - *s2);
+		}
+	s1++;
+	s2++;
+	}
+	return (0);
 }
