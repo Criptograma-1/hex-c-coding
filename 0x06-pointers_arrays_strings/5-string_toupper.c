8c2960d6f8fb54a36982669d50284e29d3cc4945
Criptograma-1 <andresa.info@gmail.com>
2021-04-09 22:19:58 +0000

    Add task 5 files

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* string_toupper - changes all lowercase letters of a string to uppercase
+* @s: string
+* Return: s
+*/
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}
+		i++;
+	}
+	return (s);
+}
