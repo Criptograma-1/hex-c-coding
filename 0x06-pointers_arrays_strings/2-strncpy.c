93b404c297a334d6a04cd2bd23533b8acb9add03
Criptograma-1 <andresa.info@gmail.com>
2021-04-09 16:54:55 +0000

    add task 2 files

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+* _srtncat - a function that copies a strings
+* @dest: A pointer to the string
+* @src: String to be copie
+* @n: concatenates caracters
+* Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+
+        while (*(src + j) != '\0' && n > j)
+	{
+               *(dest + j) = *(src + j);
+		j++;
+	}
+	while (n > j)
+	{
+		*(dest + j) = '\0';
+		j++;
+	}
+	return (dest);
+}
