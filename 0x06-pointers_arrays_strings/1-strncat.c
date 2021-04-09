462c45ae779fb6b1a3f5d4c4d737377fad2440c4
Criptograma-1 <andresa.info@gmail.com>
2021-04-09 16:24:24 +0000

    Add task 2 files

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+* _srtncat - a function that concatenates two strings
+* @dest: A pointer to the string
+* @src: String to be appended
+* @n: concatenates caracters
+* Return: dest
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+        int i, j;
+
+        i = j = 0;
+        while (*(dest + i) != '\0')
+	{
+                i++;
+	}
+
+        while (*(src + j) != '\0' && n > j)
+	{
+               *(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+        return (dest);
+}
