287592dce0578d57f56d31d1f32a5a35bf8e665d
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-04-09 16:24:53 -0300

    oh betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,13 +9,11 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
-	
 	i = j = 0;
 	while (*(dest + i) != '\0')
 	{
 		i++;
 	}
-	
 	while (*(src + j) != '\0' && n > j)
 	{
 		*(dest + i) = *(src + j);
