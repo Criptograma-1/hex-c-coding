7c50f42a20e4617fa6891e76ca8837debb97abfd
Criptograma-1 <andresa.info@gmail.com>
2021-04-15 19:41:37 +0000

    Add task 2 files

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* _strlen_recursion - a function that returns the length of a string.
+* @s: string
+* Return: lenght of a string
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
+}
