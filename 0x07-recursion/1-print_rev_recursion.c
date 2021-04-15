184545d03fb70bc9a7b2062a59c7c37f538763b1
Criptograma-1 <andresa.info@gmail.com>
2021-04-15 19:22:27 +0000

    Add task 1 files

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* _print_rev_recursion - a function that prints a string in reverse.
+* @s: array or string
+* Return: 0;
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
