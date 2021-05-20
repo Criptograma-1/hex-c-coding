8fdc3f5b566dd1b3fbb62803bdb5b4efef14c69d
Criptograma-1 <andresa.info@gmail.com>
2021-05-20 16:50:53 +0000

    Task 4

---
+++ 0x09-malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* free_grid - frees a 2 dimensional grid
+* @**grid: grids
+* @heigt: grid size
+* return: 0
+*/
+
+void free_grid(int **grid, int height)
+{
+	free (grid[heigt]);
+}
