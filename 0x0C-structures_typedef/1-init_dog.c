a44eac968984eb8b55409e67cdd1d20a5e7a1665
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-12-19 14:44:59 -0300

    Fix betty

--- 0x0C-structures_typedef/1-init_dog.c
+++ 0x0C-structures_typedef/1-init_dog.c
@@ -1,19 +1,15 @@
 #include "dog.h"
-#include <stdlib.h>
 
 /**
 * init_dog - initialize dog struct
-* @d - struct
-* @name - dog's name
-* @age - dog's age
-* @owner - dog's owner
+* @d: struct
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+d->name = name;
+d->age = age;
+d->owner = owner;
 }
