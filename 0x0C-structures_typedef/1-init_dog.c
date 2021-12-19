55262a89b3cd2c771900b4aace3daee5585760b2
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-12-19 14:28:50 -0300

    Fix task 1

--- 0x0C-structures_typedef/1-init_dog.c
+++ 0x0C-structures_typedef/1-init_dog.c
@@ -10,8 +10,9 @@
 * How initialize a strcut in C
 */
 void init_dog(struct dog *d, char *name, float age, char *owner) {
-d.name = *name;
-d.age = age;
-d.owner = *owner;
-return (0);
+struct dog *d;
+*d.name = *name;
+*d.age = age;
+*d.owner = *owner;
+
 }
