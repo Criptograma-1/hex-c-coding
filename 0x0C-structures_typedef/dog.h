2efc9f8d24737f2f2dda27b0083b258a4c92bece
Criptograma-1 <andresa.info@gmail.com>
2021-12-19 16:57:40 +0000

    Add task 0

---
+++ 0x0C-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+* struct dog - represents a dog
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+*
+* Description: How define structures
+*/
+struct dog
+{
+    char name;
+    float age;
+    char owner;
+}
