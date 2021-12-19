c0c49416ebacc90588b416eba26eeaaedb5dffd3
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-12-19 14:40:05 -0300

    Update 1-init_dog.c

--- 0x0C-structures_typedef/1-init_dog.c
+++ 0x0C-structures_typedef/1-init_dog.c
@@ -7,9 +7,12 @@
 * @age - dog's age
 * @owner - dog's owner
 */
-void init_dog(struct dog *d, char *name, float age, char *owner) 
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-d->name = name;
-d->age = age;
-d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
