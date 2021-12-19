7b2c7256e076dd695967cbd9a1baae2c3a21c038
Criptograma-1 <andresa.info@gmail.com>
2021-12-19 17:24:42 +0000

    Add task 1

---
+++ 0x0C-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+* init_dog - initialize dog struct
+* @d - struct
+* @name - dog's name
+* @age - dog's age
+* @owner - dog's owner
+*
+* How initialize a strcut in C
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner) {
+*d.name = *name;
+*d.age = age;
+*d.owner = *owner;
+return (0);
+}
