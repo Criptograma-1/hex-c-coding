69b70d5ba7c936e40507acd0b58ef51ed37d2aac
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-12-20 08:30:29 -0300

    include documentation

--- 0x0D-singly_linked_lists/lists.h
+++ 0x0D-singly_linked_lists/lists.h
@@ -3,6 +3,15 @@
 
 #include <stdlib.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *              for Holberton project
+ */
 typedef struct list_s
 {
 	char *str;
