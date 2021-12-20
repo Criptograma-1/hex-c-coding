65b3c3d79f0ce77f5476d195c0a8128ad1e3632e
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-12-20 08:36:59 -0300

    Fix betty

--- 0x0D-singly_linked_lists/1-list_len.c
+++ 0x0D-singly_linked_lists/1-list_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * list_len - A function that returns the 
- *	      number of elements in a linked list_t list.
- * @h: The linked list.
- *
- * Return: The number of elements in h.
- */
+* list_len - A function that returns the 
+*	number of elements in a linked list_t list.
+* @h: The linked list.
+*
+* Return: The number of elements in h.
+*/
 size_t list_len(const list_t *h)
 {
 
